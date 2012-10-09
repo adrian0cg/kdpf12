29b48215df8e0f54fe238ceb854df8a2323f1513
Adrian Hass <adrian.hass@googlemail.com>
2012-10-09 13:01:01 +0200

    basic setup of CaffeineLevel-Stuff (domain/services)

---
+++ grails-app/domain/de/kaufda/plat_forms/cafman/CaffeineIntake.groovy
@@ -0,0 +1,25 @@
+package de.kaufda.plat_forms.cafman
+
+/**
+ * Represents a dose of caffeine that a user takes (i.e. cup of coffee)
+ * @Plat_Forms M
+ */
+class CaffeineIntake {
+
+    /**
+     * Timestamp of consuption
+     */
+    Date taken = new Date()
+    /**
+     * The dose of this Caffeine Intake in [mg]
+     */
+    Integer dose = 150
+    /**
+     * The User that consumes the Caffeine
+     */
+    User consumer
+
+    static constraints = {
+        dose(min: 0)
+    }
+}
---
+++ test/unit/de/kaufda/plat_forms/cafman/CaffeineLevelAlertTests.groovy
@@ -0,0 +1,17 @@
+package de.kaufda.plat_forms.cafman
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(CaffeineLevelAlert)
+class CaffeineLevelAlertTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
