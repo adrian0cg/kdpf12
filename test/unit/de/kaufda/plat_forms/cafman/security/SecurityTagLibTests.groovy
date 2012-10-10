ae2bc3d4b8a692acfaec13280daadc3409fbc24f
Patrick Jungermann <patrick.jungermann@gmail.com>
2012-10-10 14:32:21 +0200

    unit tests for the SecurityTagLib

--- test/unit/de/kaufda/plat_forms/cafman/security/SecurityTagLibTests.groovy
+++ test/unit/de/kaufda/plat_forms/cafman/security/SecurityTagLibTests.groovy
@@ -1,6 +1,10 @@
 package de.kaufda.plat_forms.cafman.security
 
+import de.kaufda.plat_forms.cafman.User
+import grails.plugins.springsecurity.SpringSecurityService
+import grails.test.GrailsMock
 import grails.test.mixin.TestFor
+import org.junit.Test
 
 /**
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
@@ -10,7 +14,46 @@ import grails.test.mixin.TestFor
 @TestFor(SecurityTagLib)
 class SecurityTagLibTests {
 
-    void testSomething() {
-        fail "Implement me"
+    @Test
+    public void userFullName_notLoggedIn_renderNothing() {
+        GrailsMock control = mockFor(SpringSecurityService)
+        control.demand.isLoggedIn {-> return false}
+        tagLib.springSecurityService = control.createMock()
+
+        assert applyTemplate('<sec:userFullName/>') == ''
     }
+
+    @Test
+    public void userFullName_loggedIn_renderEncodedFullName() {
+        String fullName = "full <> name"
+        User user = new User(fullName: fullName)
+        GrailsMock control = mockFor(SpringSecurityService)
+        control.demand.isLoggedIn {-> return true}
+        control.demand.getCurrentUser {-> return user }
+        tagLib.springSecurityService = control.createMock()
+
+        assert applyTemplate('<sec:userFullName/>') == 'full &lt;&gt; name'
+    }
+
+    @Test
+    public void userEmail_notLoggedIn_renderNothing() {
+        GrailsMock control = mockFor(SpringSecurityService)
+        control.demand.isLoggedIn {-> return false}
+        tagLib.springSecurityService = control.createMock()
+
+        assert applyTemplate('<sec:userEmail/>') == ''
+    }
+
+    @Test
+    public void userEmail_loggedIn_renderEncodedEmail() {
+        String email = "email<at>example.com"
+        User user = new User(email: email)
+        GrailsMock control = mockFor(SpringSecurityService)
+        control.demand.isLoggedIn {-> return true}
+        control.demand.getCurrentUser {-> return user }
+        tagLib.springSecurityService = control.createMock()
+
+        assert applyTemplate('<sec:userEmail/>') == 'email&lt;at&gt;example.com'
+    }
+
 }
